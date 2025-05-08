420c33dd5281ccd2b237185765a7578b6c911054
Assaf Shuval <57865781+shuvalassaf@users.noreply.github.com>
2025-05-08 09:06:17 +0300

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
 require('./assets/js/pos.js');
 require('./assets/js/product-filter.js');
+require('./assets/js/customer-management.js');
 require('print-js');
--- start.js
+++ start.js
@@ -1,16 +1,5 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en from './locales/en.json';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: { en: { translation: en } },
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false },
-  });
-const setupEvents = require('./installers/setupEvents')
+const setupEvents = require('./installers/setupEvents');
 
  if (setupEvents.handleSquirrelEvent()) {
     return;
